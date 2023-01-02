75dc81b0e5df2888299fb254e41aca7a81be1492
Shaurya Dahiya <46405665+ShauryaDahiya1998@users.noreply.github.com>
2023-01-01 21:09:55 -0600

    Create problem2.cpp

---
+++ problem2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0,fl=0,maxiSum = nums[0];
+        for(int i=0;i<nums.size();i++) {
+            if(nums[i]>0) {
+                fl=1;
+            }
+        }
+        if(fl==0) {
+            for(int i=0;i<nums.size();i++) {
+                if(maxiSum<nums[i]) {
+                    maxiSum = nums[i];
+                }
+            }
+            if(maxiSum<0) {
+                return maxiSum;
+            }
+        }
+        
+        for(int i=0;i<nums.size();i++) {
+            sum+=nums[i];
+            if(sum<0) {
+                sum = 0;
+            }
+            if(sum>maxiSum) {
+                maxiSum = sum;
+            }
+        }
+        return maxiSum;
+    }
+};
